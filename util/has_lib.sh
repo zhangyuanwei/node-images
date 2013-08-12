c4b1b1610571a3c33c9e44c349e9630a4f49320e
xiangshouding <fansekey@gmail.com>
2013-08-12 15:04:27 +0800

    bugfix 某些linux，which命令输出到了stderr导致无法拦截错误

--- util/has_lib.sh
+++ util/has_lib.sh
@@ -12,7 +12,7 @@ has_lib() {
   fi
 
   # Try using ldconfig on linux systems
-  for LINE in `which ldconfig > /dev/null && ldconfig -p 2>/dev/null | grep -E $regex`; do
+  for LINE in `which ldconfig 2>&1 >/dev/null && ldconfig -p 2>/dev/null | grep -E $regex`; do
     return 0
   done
 
