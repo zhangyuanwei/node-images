8cf6d449b74cc4958253affb0305e61e3967397d
liaoxuezhi <liaoxuezhi@baidu.com>
2014-03-17 14:34:58 +0800

    add some test files

--- test/index.js
+++ test/index.js
@@ -4,10 +4,26 @@ images("input.png")
     .resize( 200 )
     .save("output_new.png");
 
+
 images("input.png")
     .size( 200 )
     .save("output_old.png");
 
-// images("input.png")
-//     .resize( null, 400 )
-//     .save("output_100%x400.png");
+
+images("input.jpg")
+    .resize( 200 )
+    .save("output_new.jpg");
+
+
+images("input.jpg")
+    .size( 200 )
+    .save("output_old.jpg");
+
+images("input.gif")
+    .resize( 200 )
+    .save("output_new_gif.jpg");
+
+
+images("input.gif")
+    .size( 200 )
+    .save("output_old_gif.jpg");
