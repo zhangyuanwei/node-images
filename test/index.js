1b3cd79b65e141694bf7ee9e09b1acfef8945793
liaoxuezhi <liaoxuezhi@baidu.com>
2014-03-17 13:31:28 +0800

    add comprare

--- test/index.js
+++ test/index.js
@@ -2,8 +2,12 @@ var images = require("../");
 
 images("input.png")
     .resize( 200 )
-    .save("output_200x100%.png");
+    .save("output_new.png");
 
 images("input.png")
-    .resize( null, 400 )
-    .save("output_100%x400.png");
+    .size( 200 )
+    .save("output_old.png");
+
+// images("input.png")
+//     .resize( null, 400 )
+//     .save("output_100%x400.png");
