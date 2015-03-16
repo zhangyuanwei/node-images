233b0b4f84b03e13b6b38bf232ecab8ef71ef56a
xiangshouding <fansekey@gmail.com>
2015-03-16 13:33:47 +0800

    不再安装时下载对应二进制包，而是直接放到 NPM 包中

--- binding.js
+++ binding.js
@@ -1,5 +1,45 @@
-try {
-   module.exports = require('./build/Release/binding.node');
-} catch ( e ) {
-    throw new Error('Can\'t load the addon. please try to run `node install.js` command.');
+
+'use strict';
+
+function compiler(a, b) {
+
+    if (!/^(?:\d+.?)+$/.test(a) || !/^(?:\d+.?)+/.test(b)) {
+        return a > b;
+    }
+
+    var aArr = a.split('.');
+    var bArr = b.split('.');
+    var max = Math.max(aArr.length, bArr.length);
+
+    for (var i = 0; i < max; i++) {
+        if ((aArr[i] && !bArr[i])  || aArr[i] > bArr[i]) {
+            return 1;
+        } else if ((!aArr[i] && bArr[i]) || aArr[i] < bArr[i]) {
+            return -1;
+        }
+    }
+
+    return 0;
 }
+
+var bindingMap = require('./package.json').bindingMap;
+
+for ( var i in bindingMap)  {
+    if (bindingMap.hasOwnProperty(i)) {
+        var target = i;
+        var versions = bindingMap[i];
+
+        var cur = process.versions['node'];
+
+        if (compiler(versions[0], cur) <= 0 && compiler(versions[1], cur) >= 0) {
+            try {
+                module.exports = require('./bindings/'+ process.platform + '/' + process.arch + '/' + target + '/binding.node');
+                return;
+            } catch ( e ) {
+                throw new Error('Can\'t load the addon. Issue to: https://github.com/zhangyuanwei/issues. ' + e.stack);
+            }
+        }
+    }
+}
+
+throw new Error('Can\'t load the addon. Issue to: https://github.com/zhangyuanwei/issues.');
